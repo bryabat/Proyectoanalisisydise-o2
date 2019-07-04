863b8955d64371a69f2dfa71bbc258f65003bf69
Abraham456 <pedro.cotoc777@gmail.com>
2019-07-03 23:13:50 -0600

    añadi clases
    añadi jpa's

---
+++ proyectoanalisi/src/main/java/Analisis/DetallecomprasJpaController.java
@@ -0,0 +1,262 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analisis;
+
+import Analisis.exceptions.IllegalOrphanException;
+import Analisis.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import Bases.Compras;
+import Bases.Detallecompras;
+import Bases.Producto;
+import Bases.Devoluciones;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author pedro
+ */
+public class DetallecomprasJpaController implements Serializable {
+
+    public DetallecomprasJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Detallecompras detallecompras) {
+        if (detallecompras.getDevolucionesCollection() == null) {
+            detallecompras.setDevolucionesCollection(new ArrayList<Devoluciones>());
+        }
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Compras comprasIdcompras = detallecompras.getComprasIdcompras();
+            if (comprasIdcompras != null) {
+                comprasIdcompras = em.getReference(comprasIdcompras.getClass(), comprasIdcompras.getIdcompras());
+                detallecompras.setComprasIdcompras(comprasIdcompras);
+            }
+            Producto productoIdproducto = detallecompras.getProductoIdproducto();
+            if (productoIdproducto != null) {
+                productoIdproducto = em.getReference(productoIdproducto.getClass(), productoIdproducto.getIdproducto());
+                detallecompras.setProductoIdproducto(productoIdproducto);
+            }
+            Collection<Devoluciones> attachedDevolucionesCollection = new ArrayList<Devoluciones>();
+            for (Devoluciones devolucionesCollectionDevolucionesToAttach : detallecompras.getDevolucionesCollection()) {
+                devolucionesCollectionDevolucionesToAttach = em.getReference(devolucionesCollectionDevolucionesToAttach.getClass(), devolucionesCollectionDevolucionesToAttach.getIddevoluciones());
+                attachedDevolucionesCollection.add(devolucionesCollectionDevolucionesToAttach);
+            }
+            detallecompras.setDevolucionesCollection(attachedDevolucionesCollection);
+            em.persist(detallecompras);
+            if (comprasIdcompras != null) {
+                comprasIdcompras.getDetallecomprasCollection().add(detallecompras);
+                comprasIdcompras = em.merge(comprasIdcompras);
+            }
+            if (productoIdproducto != null) {
+                productoIdproducto.getDetallecomprasCollection().add(detallecompras);
+                productoIdproducto = em.merge(productoIdproducto);
+            }
+            for (Devoluciones devolucionesCollectionDevoluciones : detallecompras.getDevolucionesCollection()) {
+                Detallecompras oldDetallecomprasIdOfDevolucionesCollectionDevoluciones = devolucionesCollectionDevoluciones.getDetallecomprasId();
+                devolucionesCollectionDevoluciones.setDetallecomprasId(detallecompras);
+                devolucionesCollectionDevoluciones = em.merge(devolucionesCollectionDevoluciones);
+                if (oldDetallecomprasIdOfDevolucionesCollectionDevoluciones != null) {
+                    oldDetallecomprasIdOfDevolucionesCollectionDevoluciones.getDevolucionesCollection().remove(devolucionesCollectionDevoluciones);
+                    oldDetallecomprasIdOfDevolucionesCollectionDevoluciones = em.merge(oldDetallecomprasIdOfDevolucionesCollectionDevoluciones);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Detallecompras detallecompras) throws IllegalOrphanException, NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Detallecompras persistentDetallecompras = em.find(Detallecompras.class, detallecompras.getId());
+            Compras comprasIdcomprasOld = persistentDetallecompras.getComprasIdcompras();
+            Compras comprasIdcomprasNew = detallecompras.getComprasIdcompras();
+            Producto productoIdproductoOld = persistentDetallecompras.getProductoIdproducto();
+            Producto productoIdproductoNew = detallecompras.getProductoIdproducto();
+            Collection<Devoluciones> devolucionesCollectionOld = persistentDetallecompras.getDevolucionesCollection();
+            Collection<Devoluciones> devolucionesCollectionNew = detallecompras.getDevolucionesCollection();
+            List<String> illegalOrphanMessages = null;
+            for (Devoluciones devolucionesCollectionOldDevoluciones : devolucionesCollectionOld) {
+                if (!devolucionesCollectionNew.contains(devolucionesCollectionOldDevoluciones)) {
+                    if (illegalOrphanMessages == null) {
+                        illegalOrphanMessages = new ArrayList<String>();
+                    }
+                    illegalOrphanMessages.add("You must retain Devoluciones " + devolucionesCollectionOldDevoluciones + " since its detallecomprasId field is not nullable.");
+                }
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            if (comprasIdcomprasNew != null) {
+                comprasIdcomprasNew = em.getReference(comprasIdcomprasNew.getClass(), comprasIdcomprasNew.getIdcompras());
+                detallecompras.setComprasIdcompras(comprasIdcomprasNew);
+            }
+            if (productoIdproductoNew != null) {
+                productoIdproductoNew = em.getReference(productoIdproductoNew.getClass(), productoIdproductoNew.getIdproducto());
+                detallecompras.setProductoIdproducto(productoIdproductoNew);
+            }
+            Collection<Devoluciones> attachedDevolucionesCollectionNew = new ArrayList<Devoluciones>();
+            for (Devoluciones devolucionesCollectionNewDevolucionesToAttach : devolucionesCollectionNew) {
+                devolucionesCollectionNewDevolucionesToAttach = em.getReference(devolucionesCollectionNewDevolucionesToAttach.getClass(), devolucionesCollectionNewDevolucionesToAttach.getIddevoluciones());
+                attachedDevolucionesCollectionNew.add(devolucionesCollectionNewDevolucionesToAttach);
+            }
+            devolucionesCollectionNew = attachedDevolucionesCollectionNew;
+            detallecompras.setDevolucionesCollection(devolucionesCollectionNew);
+            detallecompras = em.merge(detallecompras);
+            if (comprasIdcomprasOld != null && !comprasIdcomprasOld.equals(comprasIdcomprasNew)) {
+                comprasIdcomprasOld.getDetallecomprasCollection().remove(detallecompras);
+                comprasIdcomprasOld = em.merge(comprasIdcomprasOld);
+            }
+            if (comprasIdcomprasNew != null && !comprasIdcomprasNew.equals(comprasIdcomprasOld)) {
+                comprasIdcomprasNew.getDetallecomprasCollection().add(detallecompras);
+                comprasIdcomprasNew = em.merge(comprasIdcomprasNew);
+            }
+            if (productoIdproductoOld != null && !productoIdproductoOld.equals(productoIdproductoNew)) {
+                productoIdproductoOld.getDetallecomprasCollection().remove(detallecompras);
+                productoIdproductoOld = em.merge(productoIdproductoOld);
+            }
+            if (productoIdproductoNew != null && !productoIdproductoNew.equals(productoIdproductoOld)) {
+                productoIdproductoNew.getDetallecomprasCollection().add(detallecompras);
+                productoIdproductoNew = em.merge(productoIdproductoNew);
+            }
+            for (Devoluciones devolucionesCollectionNewDevoluciones : devolucionesCollectionNew) {
+                if (!devolucionesCollectionOld.contains(devolucionesCollectionNewDevoluciones)) {
+                    Detallecompras oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones = devolucionesCollectionNewDevoluciones.getDetallecomprasId();
+                    devolucionesCollectionNewDevoluciones.setDetallecomprasId(detallecompras);
+                    devolucionesCollectionNewDevoluciones = em.merge(devolucionesCollectionNewDevoluciones);
+                    if (oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones != null && !oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones.equals(detallecompras)) {
+                        oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones.getDevolucionesCollection().remove(devolucionesCollectionNewDevoluciones);
+                        oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones = em.merge(oldDetallecomprasIdOfDevolucionesCollectionNewDevoluciones);
+                    }
+                }
+            }
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = detallecompras.getId();
+                if (findDetallecompras(id) == null) {
+                    throw new NonexistentEntityException("The detallecompras with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws IllegalOrphanException, NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Detallecompras detallecompras;
+            try {
+                detallecompras = em.getReference(Detallecompras.class, id);
+                detallecompras.getId();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The detallecompras with id " + id + " no longer exists.", enfe);
+            }
+            List<String> illegalOrphanMessages = null;
+            Collection<Devoluciones> devolucionesCollectionOrphanCheck = detallecompras.getDevolucionesCollection();
+            for (Devoluciones devolucionesCollectionOrphanCheckDevoluciones : devolucionesCollectionOrphanCheck) {
+                if (illegalOrphanMessages == null) {
+                    illegalOrphanMessages = new ArrayList<String>();
+                }
+                illegalOrphanMessages.add("This Detallecompras (" + detallecompras + ") cannot be destroyed since the Devoluciones " + devolucionesCollectionOrphanCheckDevoluciones + " in its devolucionesCollection field has a non-nullable detallecomprasId field.");
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            Compras comprasIdcompras = detallecompras.getComprasIdcompras();
+            if (comprasIdcompras != null) {
+                comprasIdcompras.getDetallecomprasCollection().remove(detallecompras);
+                comprasIdcompras = em.merge(comprasIdcompras);
+            }
+            Producto productoIdproducto = detallecompras.getProductoIdproducto();
+            if (productoIdproducto != null) {
+                productoIdproducto.getDetallecomprasCollection().remove(detallecompras);
+                productoIdproducto = em.merge(productoIdproducto);
+            }
+            em.remove(detallecompras);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Detallecompras> findDetallecomprasEntities() {
+        return findDetallecomprasEntities(true, -1, -1);
+    }
+
+    public List<Detallecompras> findDetallecomprasEntities(int maxResults, int firstResult) {
+        return findDetallecomprasEntities(false, maxResults, firstResult);
+    }
+
+    private List<Detallecompras> findDetallecomprasEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Detallecompras.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Detallecompras findDetallecompras(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Detallecompras.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getDetallecomprasCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Detallecompras> rt = cq.from(Detallecompras.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
---
+++ proyectoanalisi/src/main/java/Analisis/ProveedorJpaController.java
@@ -0,0 +1,204 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Analisis;
+
+import Analisis.exceptions.IllegalOrphanException;
+import Analisis.exceptions.NonexistentEntityException;
+import java.io.Serializable;
+import javax.persistence.Query;
+import javax.persistence.EntityNotFoundException;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import Bases.Compras;
+import Bases.Proveedor;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+/**
+ *
+ * @author pedro
+ */
+public class ProveedorJpaController implements Serializable {
+
+    public ProveedorJpaController(EntityManagerFactory emf) {
+        this.emf = emf;
+    }
+    private EntityManagerFactory emf = null;
+
+    public EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    public void create(Proveedor proveedor) {
+        if (proveedor.getComprasCollection() == null) {
+            proveedor.setComprasCollection(new ArrayList<Compras>());
+        }
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Collection<Compras> attachedComprasCollection = new ArrayList<Compras>();
+            for (Compras comprasCollectionComprasToAttach : proveedor.getComprasCollection()) {
+                comprasCollectionComprasToAttach = em.getReference(comprasCollectionComprasToAttach.getClass(), comprasCollectionComprasToAttach.getIdcompras());
+                attachedComprasCollection.add(comprasCollectionComprasToAttach);
+            }
+            proveedor.setComprasCollection(attachedComprasCollection);
+            em.persist(proveedor);
+            for (Compras comprasCollectionCompras : proveedor.getComprasCollection()) {
+                Proveedor oldProveedorIdproveedorOfComprasCollectionCompras = comprasCollectionCompras.getProveedorIdproveedor();
+                comprasCollectionCompras.setProveedorIdproveedor(proveedor);
+                comprasCollectionCompras = em.merge(comprasCollectionCompras);
+                if (oldProveedorIdproveedorOfComprasCollectionCompras != null) {
+                    oldProveedorIdproveedorOfComprasCollectionCompras.getComprasCollection().remove(comprasCollectionCompras);
+                    oldProveedorIdproveedorOfComprasCollectionCompras = em.merge(oldProveedorIdproveedorOfComprasCollectionCompras);
+                }
+            }
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void edit(Proveedor proveedor) throws IllegalOrphanException, NonexistentEntityException, Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Proveedor persistentProveedor = em.find(Proveedor.class, proveedor.getIdproveedor());
+            Collection<Compras> comprasCollectionOld = persistentProveedor.getComprasCollection();
+            Collection<Compras> comprasCollectionNew = proveedor.getComprasCollection();
+            List<String> illegalOrphanMessages = null;
+            for (Compras comprasCollectionOldCompras : comprasCollectionOld) {
+                if (!comprasCollectionNew.contains(comprasCollectionOldCompras)) {
+                    if (illegalOrphanMessages == null) {
+                        illegalOrphanMessages = new ArrayList<String>();
+                    }
+                    illegalOrphanMessages.add("You must retain Compras " + comprasCollectionOldCompras + " since its proveedorIdproveedor field is not nullable.");
+                }
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            Collection<Compras> attachedComprasCollectionNew = new ArrayList<Compras>();
+            for (Compras comprasCollectionNewComprasToAttach : comprasCollectionNew) {
+                comprasCollectionNewComprasToAttach = em.getReference(comprasCollectionNewComprasToAttach.getClass(), comprasCollectionNewComprasToAttach.getIdcompras());
+                attachedComprasCollectionNew.add(comprasCollectionNewComprasToAttach);
+            }
+            comprasCollectionNew = attachedComprasCollectionNew;
+            proveedor.setComprasCollection(comprasCollectionNew);
+            proveedor = em.merge(proveedor);
+            for (Compras comprasCollectionNewCompras : comprasCollectionNew) {
+                if (!comprasCollectionOld.contains(comprasCollectionNewCompras)) {
+                    Proveedor oldProveedorIdproveedorOfComprasCollectionNewCompras = comprasCollectionNewCompras.getProveedorIdproveedor();
+                    comprasCollectionNewCompras.setProveedorIdproveedor(proveedor);
+                    comprasCollectionNewCompras = em.merge(comprasCollectionNewCompras);
+                    if (oldProveedorIdproveedorOfComprasCollectionNewCompras != null && !oldProveedorIdproveedorOfComprasCollectionNewCompras.equals(proveedor)) {
+                        oldProveedorIdproveedorOfComprasCollectionNewCompras.getComprasCollection().remove(comprasCollectionNewCompras);
+                        oldProveedorIdproveedorOfComprasCollectionNewCompras = em.merge(oldProveedorIdproveedorOfComprasCollectionNewCompras);
+                    }
+                }
+            }
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = proveedor.getIdproveedor();
+                if (findProveedor(id) == null) {
+                    throw new NonexistentEntityException("The proveedor with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public void destroy(Integer id) throws IllegalOrphanException, NonexistentEntityException {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            Proveedor proveedor;
+            try {
+                proveedor = em.getReference(Proveedor.class, id);
+                proveedor.getIdproveedor();
+            } catch (EntityNotFoundException enfe) {
+                throw new NonexistentEntityException("The proveedor with id " + id + " no longer exists.", enfe);
+            }
+            List<String> illegalOrphanMessages = null;
+            Collection<Compras> comprasCollectionOrphanCheck = proveedor.getComprasCollection();
+            for (Compras comprasCollectionOrphanCheckCompras : comprasCollectionOrphanCheck) {
+                if (illegalOrphanMessages == null) {
+                    illegalOrphanMessages = new ArrayList<String>();
+                }
+                illegalOrphanMessages.add("This Proveedor (" + proveedor + ") cannot be destroyed since the Compras " + comprasCollectionOrphanCheckCompras + " in its comprasCollection field has a non-nullable proveedorIdproveedor field.");
+            }
+            if (illegalOrphanMessages != null) {
+                throw new IllegalOrphanException(illegalOrphanMessages);
+            }
+            em.remove(proveedor);
+            em.getTransaction().commit();
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public List<Proveedor> findProveedorEntities() {
+        return findProveedorEntities(true, -1, -1);
+    }
+
+    public List<Proveedor> findProveedorEntities(int maxResults, int firstResult) {
+        return findProveedorEntities(false, maxResults, firstResult);
+    }
+
+    private List<Proveedor> findProveedorEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(Proveedor.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public Proveedor findProveedor(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Proveedor.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    public int getProveedorCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Proveedor> rt = cq.from(Proveedor.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return ((Long) q.getSingleResult()).intValue();
+        } finally {
+            em.close();
+        }
+    }
+    
+}
